ad1e9a8e103b93db7d2d74cdd78bd51edb964223
luizhenrick-programmer <105799263+luizhenrick-programmer@users.noreply.github.com>
2024-07-17 19:38:08 -0300

    Projeto Final

---
+++ src/calculadora/Calculadora.java
@@ -0,0 +1,10 @@
+package calculadora;
+import calculadora.view.CalculadoraFrm;
+
+public class Calculadora {
+
+    public static void main(String[] args) {
+        CalculadoraFrm tela = new CalculadoraFrm();
+        tela.setVisible(true);
+    }
+}
